bd436029705deb36a5ff883815c6ab7f5121f94a
UPZDesign <143747637+UpzDesign@users.noreply.github.com>
2026-07-21 14:32:14 -0400

    Load global UI polish styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import "./admin-forms.css";
 import "./admin-requests.css";
 import "./packages.css";
 import "./projects.css";
+import "./ui-polish.css";
 import Providers from "./providers";
 import Cart from "@/components/Cart";
 
