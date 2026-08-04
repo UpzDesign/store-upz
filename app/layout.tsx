22d0b819468bc55c9eaa633e254cd1f68f278a48
UPZDesign <143747637+UpzDesign@users.noreply.github.com>
2026-08-04 13:53:21 -0400

    Load client request design system

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./portal-responsive.css";
 import "./portal-requests.css";
 import "./request-composer.css";
 import "./portal-request-cart.css";
+import "./client-request-system.css";
 import "./admin.css";
 import "./admin-forms.css";
 import "./admin-requests.css";
